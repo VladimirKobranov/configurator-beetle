0664fc342c0fe5372b7ded209c2aab7c5020042f
VladimirKobranov <josephclturok@gmail.com>
2025-06-07 00:39:44 +0900

    stage as main

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Environment,
-  Grid,
-  PerspectiveCamera,
-} from "@react-three/drei";
+import UIOverlay from "./components/UIOverlay";
 
-import Beetle from "./components/Beetle";
-
-import UIOverlay from "./components/UIOverlay"; // The separated UI component
-
-import { useStore } from "@/store";
-import { gridConfig, cameraConfig } from "@/configs/config";
-
-function Scene() {
-  const beetleRef = useRef();
-  const isGrid = useStore((state) => state.isGrid);
-  const rotateSpeed = useStore((state) => state.rotateSpeed);
-
-  const theme = useStore((state) => state.theme);
-
-  return (
-    <>
-      <PerspectiveCamera makeDefault {...cameraConfig} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 2, 2]} intensity={1} />
-      <Beetle ref={beetleRef} />
-      <color
-        attach="background"
-        args={[theme === "dark" ? "#202020" : "skyblue"]}
-      />
-      <OrbitControls
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI / 2}
-        target={[-0.02, 0.55, -0.28]}
-        minDistance={3}
-        maxDistance={10}
-        enableDamping
-        makeDefault
-        passive
-        autoRotate
-        autoRotateSpeed={rotateSpeed}
-      />
-      <Environment preset="city" />
-      {isGrid && <Grid {...gridConfig} />}
-    </>
-  );
-}
+import Scene from "./components/scene";
 
 function App() {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <Canvas className="absolute inset-0 z-0">
+      <Canvas className="absolute inset-0 z-0" shadows>
         <Scene />
       </Canvas>
-
       <UIOverlay />
     </div>
   );
---
+++ src/components/scene.jsx
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+
+import { OrbitControls, Stage, PerspectiveCamera } from "@react-three/drei";
+import Beetle from "@/components/Beetle";
+import { useStore } from "@/store";
+import { cameraConfig } from "@/configs/config";
+
+const Scene = () => {
+  const beetleRef = useRef();
+  const rotateSpeed = useStore((state) => state.rotateSpeed);
+  const theme = useStore((state) => state.theme);
+
+  return (
+    <>
+      <PerspectiveCamera makeDefault {...cameraConfig} />
+      <color
+        attach="background"
+        args={[theme === "dark" ? "#202020" : "skyblue"]}
+      />
+      <Stage intensity={0.5} shadows="contact" environment="city">
+        <Beetle ref={beetleRef} />
+      </Stage>
+      <OrbitControls
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
+        target={[-0.02, 0.55, -0.28]}
+        minDistance={3}
+        maxDistance={10}
+        enableDamping
+        makeDefault
+        passive
+        autoRotate
+        autoRotateSpeed={rotateSpeed}
+      />
+    </>
+  );
+};
+
+export default Scene;
