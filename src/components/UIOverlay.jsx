5271d68519eeafa46006e10487c4367ace2f0d39
VladimirKobranov <josephclturok@gmail.com>
2025-06-06 19:38:43 +0900

    min ui

--- src/components/UIOverlay.jsx
+++ src/components/UIOverlay.jsx
@@ -1,11 +1,12 @@
 import { useStore } from "@/store";
-import React from "react";
+import React, { useState } from "react";
 
 import { ThemeToggle } from "./themeToggler";
 
 import { Slider } from "@/components/ui/slider";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
+import { Button } from "./ui/button";
 
 export default function UIOverlay() {
   const rotateSpeed = useStore((state) => state.rotateSpeed);
@@ -13,17 +14,18 @@ export default function UIOverlay() {
   const isGrid = useStore((state) => state.isGrid);
   const handleGridVisibility = useStore((state) => state.handleGridVisibility);
 
+  const [tab, setTab] = useState("default");
+
+  const handleTab = (val) => {
+    setTab((prev) => (prev === val ? "default" : val));
+  };
+
   return (
     <div className="absolute top-0 left-0 z-10 m-4">
-      <div className="text-left space-y-4 bg-white/80 backdrop-blur p-6 rounded-xl shadow-lg">
-        <h1 className="text-xl font-bold text-gray-800">Vite + React</h1>
+      <div className="text-left space-y-4 bg-white/80 backdrop-blur p-6 rounded-xl shadow-lg mb-2">
+        <h1 className="text-xl font-bold text-gray-800">Car configurator</h1>
 
         <div className="space-y-4">
-          <div className="flex items-center space-x-2">
-            <Switch onClick={handleGridVisibility} />
-            <Label htmlFor="airplane-mode">Grid {isGrid ? "On" : "Off"}</Label>
-          </div>
-
           <Slider
             value={[rotateSpeed]} // make it controlled
             max={5}
@@ -37,12 +39,43 @@ export default function UIOverlay() {
           )}
         </div>
 
-        <p className="text-xs text-gray-600">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-
-        <ThemeToggle />
+        <div className="flex flex-col gap-2">
+          <Button onClick={() => handleTab("material")}>Materials</Button>
+          <Button onClick={() => handleTab("parts")}>Parts</Button>
+          <Button onClick={() => handleTab("extra")}>Extra</Button>
+        </div>
       </div>
+
+      {tab !== "default" && (
+        <div className="text-left space-y-4 bg-white/80 backdrop-blur p-6 rounded-xl shadow-lg">
+          {tab === "material" && (
+            <div>
+              <h2>Material Options...</h2>
+            </div>
+          )}
+          {tab === "parts" && (
+            <div>
+              <h2>Parts Options...</h2>
+            </div>
+          )}
+          {tab === "extra" && (
+            <div className="space-y-4">
+              <h2> Extra Options...</h2>
+              <div className="flex items-center space-x-2">
+                <Switch onClick={handleGridVisibility} />
+                <Label htmlFor="airplane-mode">
+                  Grid {isGrid ? "On" : "Off"}
+                </Label>
+              </div>
+              <ThemeToggle />
+
+              <p className="text-xs text-gray-600">
+                Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
