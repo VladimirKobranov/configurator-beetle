60513bd0f0335532066723211d75b62e8f39eaf9
VladimirKobranov <josephclturok@gmail.com>
2025-06-08 12:13:07 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const env = loadEnv(process.cwd(), "")
-const PORT = env.VITE_PORT;
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+const PORT = Number(env.VITE_PORT) || 5173
+
 console.log('port', PORT)
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths({ projects: ['./jsconfig.json'] })
+  ],
   server: {
-    port: PORT,
+    port: PORT
   }
-
 })
